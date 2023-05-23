27d1b84224f0d3021644136498ca8c3a31fb3fe1
Maximiliano <maxifisz@gmail.com>
2023-05-22 22:29:16 -0300

    pipipip

---
+++ guia7.py
@@ -0,0 +1,34 @@
+# Ej 1
+#1.
+def raizDe2() -> float:
+    return round(2**0.5,4)
+#2.
+def imprimir_hola():
+    print("hola")
+
+#3.
+def imprimir_un_verso():
+    print("Que buen dato me tiraste mi amigo\npero no recuerdo haberte preguntado")
+
+#4.
+def factorial_2() -> int:
+    return 2
+
+#5.
+def factorial_3() -> int:
+    return 6
+
+#6.
+def factorial_4() -> int:
+    return 24
+
+#7.
+def factorial_5() -> int: 
+    return 120
+
+# Ej 2
+#1.
+def imprimir_saludo(nombre: str):
+    print(f"Hola {nombre}")
+
+imprimir_saludo("mauri")
