3fe5699ba85cc0d84541037219265e86d9ee08a7
mfisz <maxifisz@gmail.com>
2023-05-23 17:30:41 -0300

    aaaaaapipipipipi

--- guia7.py
+++ guia7.py
@@ -85,3 +85,101 @@ def es_peso_util(peso: int) -> bool:
 def sirve_pino(altura: int) -> bool:
     return es_peso_util(peso_pino(altura))
 
+#Ej 5
+#1.
+def devolver_el_doble_si_es_par(numero:int) -> int:
+    if(es_multiplo_de(numero,2)):
+        return numero*2
+    else:
+        return numero
+
+
+#2.
+def devolver_valor_si_es_par_sino_el_que_sigue(numero:int) -> int:
+    if(es_multiplo_de(numero,2)):
+        return numero
+    else:
+        return numero+1
+
+#3.
+def devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9(numero:int) -> int:
+    if(es_multiplo_de(numero, 9)):
+        return numero*3
+    elif(es_multiplo_de(numero, 3)):
+        return numero*2
+    else:
+        return numero
+
+#4.
+def juzgador_de_nombre(nombre:str) -> str:
+    if(len(nombre) >= 5):
+        return "Tu nombre tiene muchas letras"
+    else:
+        return "Tu nombre tiene menos de 5 letras"
+
+#5.
+def dios_santo(sexo:str,edad:int) -> str:
+    if(edad < 18):
+        return "playa playa con las chicas en malla"
+    elif(sexo == "f" and edad > 59):
+        return "playa moment"
+    elif(sexo == "m" and edad > 64):
+        return "playitaaaa"
+    else:
+        return "palitaaaaaaaaaaaa"
+
+#Ej 6
+#1.
+def uno_al_diez():
+    n:int = 1
+    while(n<11):
+        print(n)
+        n+=1
+
+#2.
+def pares_10_40():
+    n:int = 10
+    while(n<41):
+        if(es_multiplo_de(n,2)):
+            print(n)
+            n+=1
+        else:
+            n+=1
+
+#3.
+def ecos():
+    n:int = 0
+    while(n<10):
+        print("eco")
+        n+=1
+
+#4.
+def cohete(numero:int):
+    while(numero>0):
+        print(numero)
+        numero-=1
+    print("Despegue")
+
+#5.
+def delorean(partida:int, llegada:int):
+    while(partida>llegada):
+        partida-=1
+        print(f"Viajo un año al pasado, estamos en el año {partida}")
+
+
+def delorean_ls_swapped(partida:int, llegada:int):
+    while(partida>llegada):
+        partida-=20
+        print(f"Viajo un año al pasado, estamos en el año {partida}")
+
+
+#Ej 1
+#1.
+def for_1_al_10():
+    for num in range(1,11):
+        print(num)
+
+def for
+
+
+for_1_al_10()
