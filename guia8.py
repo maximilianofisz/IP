fd949d6430a24c9e42cd67de785aad869af5622c
mfisz <maxifisz@gmail.com>
2023-05-30 18:24:17 -0300

    ffgff

---
+++ guia8.py
@@ -0,0 +1,181 @@
+#Ej 1
+#1.
+def pertenece(elem:int, lista:[int]) -> bool:
+    for i in range(len(lista)):
+        if(lista[i]==elem):
+            return True
+    return False
+
+#2.
+def divideATodos(elem:int, lista:[int]) -> bool:
+    for i in range(len(lista)):
+        if(lista[i] % elem != 0):
+            return False
+    return True
+
+#3.
+def sumaTotal(lista:[int]) -> int:
+    sum:int = 0
+    for i in range(len(lista)):
+        sum = sum + lista[i]
+    return sum
+
+#4.
+def ordenados(lista:[int]) -> bool:
+    for i in range(len(lista)-1):
+        if(lista[i]>lista[i+1]):
+            return False
+    return True
+
+#5.
+def long_sup_7(lista:[str]) -> bool:
+    for i in range(len(lista)):
+        if(len(lista[i]) > 7):
+            return True
+    return False
+
+#6.
+def palindromo(palabra:str) -> bool:
+    listaA:[str] = list(palabra)
+    listaB:[str] = list(reversed(palabra))
+
+    for i in range(len(listaA)):
+        if(listaA[i] != listaB[i]):
+            return False
+    return True
+
+#7.
+def tieneMinus(contra:str) -> bool:
+    charArray:[str] = list(contra)
+    for i in range(len(charArray)):
+        if(charArray[i] != charArray[i].upper()):
+            return True
+    return False
+
+def tieneMayus(contra:str) -> bool:
+    charArray:[str] = list(contra)
+    for i in range(len(charArray)):
+        if(charArray[i] != charArray[i].lower()):
+            return True
+    return False
+
+def tieneNumero(contra:str) -> bool:
+    numeros:[str] = list("1234567890")
+    charArray:[str] = list(contra)
+    for i in range(len(charArray)):
+        if(pertenece(charArray[i], numeros)):
+            return True
+    return False
+
+
+def contraseña(contra:str) -> str:
+    if(len(contra) < 5):
+        return "ROJA"
+    elif(len(contra) > 8 and tieneMinus(contra) and tieneMayus(contra) and tieneNumero(contra)):
+        return "VERDE"
+    else:
+        return "AMARILLA"
+
+#8.
+def calculador_de_saldo(operaciones:[(str,int)]) -> int:
+    saldo:int = 0
+    for i in range(len(operaciones)):
+        if(operaciones[i][0]=="I"):
+            saldo = saldo + operaciones[i][1]
+        else:
+            saldo = saldo - operaciones[i][1]
+    return saldo
+
+#9.
+def vocales_distintas(palabra:str) -> bool:
+    contador:int = 0
+    vocales:[str] = list("aAeEiIoOuU")
+    palabraArray = list(palabra)
+    for i in range(len(palabraArray)):
+        if(pertenece(palabraArray[i],vocales)):
+            contador += 1
+    if(contador >= 3):
+        return True
+    return False
+
+#Ej 2
+#1.
+def pares_por_0_inout(numeros:[int]) -> [int]:
+    for i in range(len(numeros)):
+        if(i % 2 == 0):
+            numeros[i] = 0
+    return numeros
+
+#2.
+def pares_por_0_in(numeros:[int]) -> [int]:
+    nuevaLista:[int] = []
+    for i in range(len(numeros)):
+        if(i % 2 == 0):
+            nuevaLista.append(0)
+        else:
+            nuevaLista.append(numeros[i])
+    return nuevaLista
+
+#3.
+def removedor_de_vocales(palabra:str) -> str:
+    palabraOriginal:str = list(palabra)
+    palabraNueva:str = []
+    vocales:[str] = list("aAeEiIoOuU")
+
+    for i in range(len(palabraOriginal)):
+        if(not(pertenece(palabraOriginal[i],vocales))):
+            palabraNueva.append(palabraOriginal[i])
+
+    respuesta:str = ""
+
+    for i in palabraNueva:
+        respuesta+=i
+
+    return respuesta
+
+#4.
+def reemplazador_de_vocales(palabra:str) -> str:
+    palabraOriginal:str = list(palabra)
+    palabraNueva:str = []
+    vocales:[str] = list("aAeEiIoOuU")
+
+    for i in range(len(palabraOriginal)):
+        if(not(pertenece(palabraOriginal[i],vocales))):
+            palabraNueva.append(palabraOriginal[i])
+        else:
+            palabraNueva.append("_")
+
+    respuesta:str = ""
+
+    for i in palabraNueva:
+        respuesta+=i
+
+    return respuesta
+
+
+#5.
+def da_vuelta_str(palabra:str) -> str:
+    palabraOriginal:str = list(palabra)
+    palabraNueva:str = []
+
+    for i in palabraOriginal:
+        palabraNueva.append(i)
+    palabraRevertida = list(reversed(palabraNueva))
+    return palabraRevertida
+
+
+#Ej 3
+#1.
+def mis_estudiantes() -> [str]:
+    estudiantes:[str] = []
+    while(True):
+        inpet:str = input()
+        if(inpet == "listo"):
+            return estudiantes
+        else:
+            estudiantes.append(inpet)
+    return estudiantes
+
+#2.
+
+print(mis_estudiantes())
